d4f87dca2d3907dfa4cfd6d3f4c3afb1827c6e12
unknown <shuvrodevmondal337@gmail.com>
2023-05-20 15:14:24 +0600

    completed delete a toy functionality

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function run() {
       const result = await toyCollection.findOne(query);
       res.send(result);
     });
-    // Update the toy
+    // Update toy
     app.put("/updatetoy/:id", async (req, res) => {
       const id = req.params.id;
       const updatedToy = req.body;
@@ -86,6 +86,14 @@ async function run() {
       const result = await toyCollection.updateOne(filter, toy, options);
       res.send(result);
     });
+
+    // Delete toy
+    app.delete('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
